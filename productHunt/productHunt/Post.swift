3c565e39e9dd38e08f593c0350cc8c99e5a88d40
MattHew Phraxayavong <matthewphraxayavong@MattHews-MacBook-Pro.local>
2019-03-04 14:47:40 -0800

    created Post model

---
+++ productHunt/productHunt/Post.swift
@@ -0,0 +1,17 @@
+//
+//  Post.swift
+//  productHunt
+//
+//  Created by MattHew Phraxayavong on 3/4/19.
+//  Copyright © 2019 MattHew Phraxayavong. All rights reserved.
+//
+
+import Foundation
+
+struct Post {
+    let id: Int
+    let name: String
+    let tagline: String
+    let votesCount: Int
+    let commentsCount: Int
+}
