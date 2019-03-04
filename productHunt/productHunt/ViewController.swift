94020e58137623fbaf39dcdb771cc6b26df7e217
MattHew Phraxayavong <matthewphraxayavong@MattHews-MacBook-Pro.local>
2019-03-04 13:03:27 -0800

    embded nav controller and added table view

---
+++ productHunt/productHunt/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  productHunt
+//
+//  Created by MattHew Phraxayavong on 3/4/19.
+//  Copyright © 2019 MattHew Phraxayavong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
