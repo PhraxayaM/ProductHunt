17d86eeae42c5d283d8aa93d9f9992bcfda5745b
MattHew Phraxayavong <mphraxa@gmail.com>
2019-04-15 17:07:49 -0700

    Completed Post Cell view

---
+++ productHunt/productHunt/PostTableViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  PostTableViewCell.swift
+//  productHunt
+//
+//  Created by MattHew Phraxayavong on 3/16/19.
+//  Copyright © 2019 MattHew Phraxayavong. All rights reserved.
+//
+
+import UIKit
+
+class PostTableViewCell: UITableViewCell {
+
+   
+
+}
